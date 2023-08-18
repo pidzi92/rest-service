c1aae6335ff1662c268ffb145a08ebf145f6ed1c
pidzi92 <pidzi_mc@hotmail.com>
2023-08-19 01:17:30 +0200

    Update Vehicle.java

--- src/main/java/com/telemetry/restservice/entity/Vehicle.java
+++ src/main/java/com/telemetry/restservice/entity/Vehicle.java
@@ -1,16 +1,20 @@
 package com.telemetry.restservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Data
@@ -20,7 +24,6 @@ import java.io.Serializable;
 public class Vehicle implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="vehicle_id") Long vehicleId;
+    @Column(name="vehicle_id") @JsonIgnore  Long vehicleId;
     @Column(name="vehicle_sn") String vehicleSn;
-
 }
