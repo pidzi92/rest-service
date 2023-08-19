e475d72f234a33506c54078973241c58a586ab34
pidzi92 <pidzi_mc@hotmail.com>
2023-08-19 16:09:35 +0200

    Added default value for filter operation

--- src/main/java/com/telemetry/restservice/model/Filter.java
+++ src/main/java/com/telemetry/restservice/model/Filter.java
@@ -1,13 +1,12 @@
 package com.telemetry.restservice.model;
 
 import lombok.Data;
-
 import java.io.Serializable;
 
 @Data
 public class Filter implements Serializable {
 private String field;
-private FilterOperationEnum operation;
+private FilterOperationEnum operation = FilterOperationEnum.Equals;
 private Object value;
 
 }
